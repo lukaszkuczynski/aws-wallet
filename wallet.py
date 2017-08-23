d6562d3f79e07f763c856f3559c56f810c11444c
lukaszkuczynski <kuczynskilukasz@gmail.com>
2017-08-23 17:27:51 +0200

    Domain of wallet spending comes to the body of function

--- wallet.py
+++ wallet.py
@@ -1,7 +1,35 @@
-def my_handler(event, context):
-    print('Someone is invoking me!')
-    message = 'Hello {} {}!'.format(event['first_name'],
-                                    event['last_name'])
+def last_record():
     return {
-        'message' : message
+        "amount_after": 100,
+        "difference": -20,
+        "description": "bread"
     }
+
+def get_new_record(amount, description):
+    last = last_record()
+    new_record = {
+        "amount_after": last['amount_after'] - amount,
+        "description": description
+    }
+    return new_record
+
+
+def save(new_record):
+    print("Save not yet implemented")
+
+
+def my_handler(event, context):
+    new_record = {}
+    if event['op_type'] == 'spending':
+        print("received new spending")
+        last = last_record()
+        print("last time we had: %s", last)
+        new_record = get_new_record(event['amount'], event['description'])
+        print("now we will write new record: %s" % new_record)
+        save(new_record)
+
+    else:
+        raise Exception("Operation Type %s not known " % event['op_type'])
+
+
+    return new_record
